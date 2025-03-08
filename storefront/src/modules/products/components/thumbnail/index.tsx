3c8b53f52f3b26d08610096e5543a835e7a683c0
codeporx <codeporx@gmail.com>
2025-03-08 13:31:43 +0100

    square

--- storefront/src/modules/products/components/thumbnail/index.tsx
+++ storefront/src/modules/products/components/thumbnail/index.tsx
@@ -1,69 +1,70 @@
+import { Container, clx } from "@medusajs/ui"
 import Image from "next/image"
-import type { FC } from "react"
-import clx from "clsx"
+import React from "react"
+
+import PlaceholderImage from "@modules/common/icons/placeholder-image"
 
 type ThumbnailProps = {
-  image?: string
-  size?: "small" | "medium" | "large" | "full"
+  thumbnail?: string | null
+  // TODO: Fix image typings
+  images?: any[] | null
+  size?: "small" | "medium" | "large" | "full" | "square"
+  isFeatured?: boolean
   className?: string
+  "data-testid"?: string
 }
 
-const Thumbnail: FC<ThumbnailProps> = ({ image, size = "medium", className }) => {
+const Thumbnail: React.FC<ThumbnailProps> = ({
+  thumbnail,
+  images,
+  size = "square",
+  isFeatured,
+  className,
+  "data-testid": dataTestid,
+}) => {
+  const initialImage = thumbnail || images?.[0]?.url
+
   return (
-    <Container size={size} className={className}>
-      <ImageOrPlaceholder image={image} />
-    </Container>
-  )
-}
-
-type ContainerProps = {
-  size: "small" | "medium" | "large" | "full"
-  className?: string
-}
-
-const Container: FC<ContainerProps> = ({ size, className, children }) => {
-  return (
-    <div
+    <Container
       className={clx(
-        "relative w-full overflow-hidden p-4  sidebar-bg shadow-elevation-card-rest rounded-large group-hover:shadow-elevation-card-hover transition-shadow ease-in-out duration-150",
+        "relative w-full overflow-hidden p-4 sidebar-bg shadow-elevation-card-rest rounded-large group-hover:shadow-elevation-card-hover transition-shadow ease-in-out duration-150",
         className,
         {
-          "aspect-[1/1]": true, // Always make it square
+          "aspect-[11/14]": isFeatured,
+          "aspect-[9/16]": !isFeatured && size !== "square",
+          "aspect-[1/1]": size === "square",
           "w-[180px]": size === "small",
           "w-[290px]": size === "medium",
           "w-[440px]": size === "large",
           "w-full": size === "full",
-        },
+        }
       )}
+      data-testid={dataTestid}
     >
-      {children}
+      <ImageOrPlaceholder image={initialImage} size={size} />
+    </Container>
+  )
+}
+
+const ImageOrPlaceholder = ({
+  image,
+  size,
+}: Pick<ThumbnailProps, "size"> & { image?: string }) => {
+  return image ? (
+    <Image
+      src={image}
+      alt="Thumbnail"
+      className="absolute inset-0 object-cover object-center"
+      draggable={false}
+      quality={50}
+      sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
+      fill
+    />
+  ) : (
+    <div className="w-full h-full absolute inset-0 flex items-center justify-center">
+      <PlaceholderImage size={size === "small" ? 16 : 24} />
     </div>
   )
 }
 
-type ImageOrPlaceholderProps = {
-  image?: string
-}
-
-const ImageOrPlaceholder: FC<ImageOrPlaceholderProps> = ({ image }) => {
-  if (image) {
-    return (
-      <Image
-        src={image || "/placeholder.svg"}
-        alt="Thumbnail"
-        className="absolute inset-0 object-cover object-center"
-        draggable={false}
-        quality={70}
-        sizes="(max-width: 576px) 180px, (max-width: 768px) 290px, (max-width: 992px) 440px, 100vw"
-        fill
-      />
-    )
-  }
-
-  return (
-    <div className="absolute inset-0 flex items-center justify-center bg-ui-bg-default text-ui-fg-muted">No Image</div>
-  )
-}
-
 export default Thumbnail
-
