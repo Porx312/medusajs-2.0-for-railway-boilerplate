40cff3ce83f6c5ffcb4efc36af9cc34481dddba2
codeporx <codeporx@gmail.com>
2025-03-08 22:28:40 +0100

    color

--- storefront/src/components/layout/FilesLayout.tsx
+++ storefront/src/components/layout/FilesLayout.tsx
@@ -1,7 +1,7 @@
-import {  Globe, Sticker} from 'lucide-react'
+import {   Sticker} from 'lucide-react'
 import { FileExplorer } from '../FileExplorer'
-import { FaNodeJs,FaJs, FaTshirt } from "react-icons/fa";
-import { SiTypescript } from "react-icons/si";
+import { SiTypescript, SiPython, SiReact, SiAngular, SiC,SiVuedotjs  } from "react-icons/si";
+import { FaJs, FaNodeJs, FaJava } from "react-icons/fa";
 import { IoShirtOutline } from "react-icons/io5";
 
 import { PiPants } from "react-icons/pi"
@@ -21,14 +21,14 @@ const files = [
         icon: <IoShirtOutline className="h-4 w-4 text-blue-500" />,
       },
       {
-        id: "2",
+        id: "3",
         name: "Pants",
         href: "/pants",
         type: "file" as const,
         icon: <PiPants className="h-4 w-4 text-blue-500" />,
       },
       {
-        id: "2",
+        id: "4",
         name: "Stickers",
         href: "/stickers",
         type: "file" as const,
@@ -38,31 +38,74 @@ const files = [
     ],
   },
   {
-    id: "3",
+    id: "5",
     name: "Src",
     type: "folder" as const,
     children: [
   
-          {
-            id: "4",
-            name: "Typescript.ts",
-            href: "typescript",
-            type: "file" as const,
-            icon: <SiTypescript className="h-4 w-4 text-blue-400" />,
-          },
-          {
-            id: "5",
-            name: "Javascript.js",
-            href: "/javascript",
-            type: "file" as const,
-            icon: <FaJs className="h-4 w-4 text-yellow-400" />,
-          },  {
-            id: "6",
-            name: "Nodejs.js",
-            href: "/nodejs",
-            type: "file" as const,
-            icon: <FaNodeJs className="h-4 w-4 text-green-400" />,
-          }
+      {
+        id: "6",
+        name: "Typescript.ts",
+        href: "/typescript",
+        type: "file" as const,
+        icon: <SiTypescript className="h-4 w-4 text-blue-400" />,
+      },
+      {
+        id: "7",
+        name: "Javascript.js",
+        href: "/javascript",
+        type: "file" as const,
+        icon: <FaJs className="h-4 w-4 text-yellow-400" />,
+      },
+      {
+        id: "8",
+        name: "Nodejs.js",
+        href: "/nodejs",
+        type: "file" as const,
+        icon: <FaNodeJs className="h-4 w-4 text-green-400" />,
+      },
+      {
+        id: "9",
+        name: "Java.java",
+        href: "/java",
+        type: "file" as const,
+        icon: <FaJava className="h-4 w-4 text-red-600" />,
+      },
+      {
+        id: "10",
+        name: "Python.py",
+        href: "/python",
+        type: "file" as const,
+        icon: <SiPython className="h-4 w-4 text-yellow-500" />,
+      },
+      {
+        id: "11",
+        name: "React.jsx",
+        href: "/react",
+        type: "file" as const,
+        icon: <SiReact className="h-4 w-4 text-blue-500" />,
+      },
+      {
+        id: "12",
+        name: "Angular.ts",
+        href: "/angular",
+        type: "file" as const,
+        icon: <SiAngular className="h-4 w-4 text-red-500" />,
+      },
+      {
+        id: "13",
+        name: "Vue.vue",
+        href: "/vue",
+        type: "file" as const,
+        icon: <SiVuedotjs className="h-4 w-4 text-green-500" />,
+      },
+      {
+        id: "14",
+        name: "C.c",
+        href: "/c",
+        type: "file" as const,
+        icon: <SiC className="h-4 w-4 text-blue-600" />,
+      },
     ],
   }
 ]
