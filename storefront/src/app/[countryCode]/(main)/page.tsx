846d0d4cdfb4153822cd264bc9ab95360bb7729b
codeporx <codeporx@gmail.com>
2025-04-06 16:21:13 +0200

    bugs

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -6,9 +6,9 @@ import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Idestorex - Programming Accessories",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "Your one-stop shop for all your programming accessory needs.",
 }
 
 export default async function Home({
